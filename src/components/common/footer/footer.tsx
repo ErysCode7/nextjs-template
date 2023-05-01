4c764c12759a92496871aef1290fcdc763a9ffd2
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-05-01 20:28:29 +0800

    enhancement(4): footer ui enhancement

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -6,9 +6,12 @@ const Footer: NextPage<Props> = () => {
   const date = new Date().getFullYear();
 
   return (
-    <footer>
-      <div>
-        <h2>Nextjs Template ©{date}. All rights reserved</h2>
+    <footer className="w-full h-20 shadow-sm bg-white absolute bottom-0">
+      <div className="flex items-center  m-auto h-full w-[90%] lg:w-[85%]">
+        <h2 className="text-gray-500 text-xs sm:text-base">
+          Nextjs Template <span className="text-blue-500">©{date}.</span> All
+          rights reserved
+        </h2>
       </div>
     </footer>
   );
--- src/components/common/navbar/navbar.tsx
+++ src/components/common/navbar/navbar.tsx
@@ -31,7 +31,7 @@ const Navbar: NextPage<Props> = () => {
 
   return (
     <nav className="w-full h-20 shadow-sm bg-white">
-      <div className="flex items-center justify-between  m-auto h-full w-[90%] lg:w-[85%]">
+      <div className="flex items-center justify-between m-auto h-full w-[90%] lg:w-[85%]">
         <div>
           <h2 className="text-base md:text-xl lg:text-3xl text-blue-500 font-bold">
             Next.js Template
@@ -40,7 +40,7 @@ const Navbar: NextPage<Props> = () => {
 
         {/* MOBILE TO 1023px */}
         <ul
-          className={`absolute w-60 sm:w-96  ${
+          className={`absolute w-60 sm:w-96 z-50 ${
             showMobileNavbar ? "left-0" : "left-[-999px]"
           } top-0 bottom-0 transition-all duration-500 lg:hidden bg-white shadow-md flex flex-col items-center`}
         >
