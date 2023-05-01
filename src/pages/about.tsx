130844c8207edf39fd137476e09a9f997af881ba
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-05-01 20:46:51 +0800

    enhancement(6): enhance page routes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,13 @@ import type { NextPage } from "next";
 type Props = {};
 
 const About: NextPage<Props> = () => {
-  return <div>About</div>;
+  return (
+    <div className="flex gap-2 md:gap-3 lg:gap-5 items-center justify-center h-[calc(100vh_-_80px)]">
+      <h2 className="text-2xl sm:text-3xl md:text-5xl lg:text-7xl font-bold text-blue-500">
+        About Page
+      </h2>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,13 @@ import type { NextPage } from "next";
 type Props = {};
 
 const Home: NextPage<Props> = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex gap-2 md:gap-3 lg:gap-5 items-center justify-center h-[calc(100vh_-_80px)]">
+      <h1 className="text-2xl sm:text-3xl md:text-5xl lg:text-7xl font-bold text-blue-500">
+        Next.js Template
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
