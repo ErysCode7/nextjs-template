fa13c7a6bc621b60b6d2daa27076a833d3ca79fa
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-05-06 23:24:14 +0800

    enhancement(19): added nprogress package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,10 +7,30 @@ import {
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import { useEffect, useState } from "react";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
+
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
