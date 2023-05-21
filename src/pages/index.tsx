651224ca5397e11add592aab2bea22dd835c06ec
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-05-21 15:34:55 +0800

    enhancement(32): added usehooks package

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage<Props> = () => {
   return (
     <div className="flex gap-2 md:gap-3 lg:gap-5 items-center justify-center h-[calc(100vh_-_80px)]">
       <h1 className="text-2xl sm:text-3xl md:text-5xl lg:text-7xl font-bold text-blue-500">
-        Next.js Template haha
+        Next.js Template
       </h1>
     </div>
   );
