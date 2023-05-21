80588b8f8f3586a05b54a186790e71886f1ec267
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-05-21 14:52:10 +0800

    enhancement(29): add helper.ts

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,20 +1,5 @@
-import millify from 'millify';
-import moment from 'moment';
-
 export const ROUTES = {
   HOME: '/',
   ABOUT: '/todos',
   PRODUCTS: '/products',
 } as const;
-
-// USAGE: numberFormatter(200000) -> 200,000
-export const numberFormatter = (value: string | number = '') =>
-  value && value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-
-// https://momentjs.com/
-export const dateFormatter = (value: string | Date, dateFormat: string) =>
-  value !== undefined && moment(value).format(dateFormat);
-
-// https://www.npmjs.com/package/millify
-// USAGE: millifyFormatter(200000) -> 200k
-export const millifyFormatter = (value: number) => millify(value);
---
+++ src/utils/helper.ts
@@ -0,0 +1,14 @@
+import millify from 'millify';
+import moment from 'moment';
+
+// USAGE: numberFormatter(200000) -> 200,000
+export const numberFormatter = (value: string | number = '') =>
+  value && value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+// https://momentjs.com/
+export const dateFormatter = (value: string | Date, dateFormat: string) =>
+  value !== undefined && moment(value).format(dateFormat);
+
+// https://www.npmjs.com/package/millify
+// USAGE: millifyFormatter(200000) -> 200k
+export const millifyFormatter = (value: number) => millify(value);
