ce82ce9698411b6cfdaa6ce89753a0c44c53e06e
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-05-21 14:57:33 +0800

    enhancement(30): tailiwnd config breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
+    screens: {
+      mobile: '425px',
+      // => @media (min-width: 425px) { ... }
+
+      smbt: '576px',
+      // => @media (min-width: 576px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      laptop: '992px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      standard: '1440px',
+      // => @media (min-width: 1440px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+      '18k': '1800px',
+      // => @media (min-width: 1800px) { ... }
+
+      '2k': '2000px',
+      // => @media (min-width: 2000px) { ... }
+    },
   },
   plugins: [],
 };
