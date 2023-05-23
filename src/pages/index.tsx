f7dbd68ade6576481b62610c1f10048902fed300
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-05-23 21:14:30 +0800

    enhancement(39): fix homepage responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,11 @@ type Props = {};
 
 const Home: NextPage<Props> = () => {
   return (
-    <div className="flex gap-2 md:gap-3 lg:gap-5 items-center justify-center h-[calc(100vh_-_80px)]">
-      <Image src={'/next.svg'} alt="next image" width={300} height={300} />
+    <div className="flex flex-col md:flex-row gap-2 md:gap-3 lg:gap-5 items-center justify-center h-[calc(100vh_-_80px)]">
+      <div className="relative h-[70px] w-[250px]  md:h-[300px] mobile:w-[300px]">
+        <Image src={'/next.svg'} alt="next image" fill />
+      </div>
+
       <h1 className="text-2xl sm:text-3xl md:text-5xl lg:text-7xl font-bold text-blue-500">
         Template
       </h1>
